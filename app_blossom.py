e835d6f32ad1f6fb657d9aeb56ac77bf86ffe566
Lucas Bibiano <lucas.bibianot@gmail.com>
2020-02-07 21:04:05 -0300

    Gráfico de testes.

--- app_blossom.py
+++ app_blossom.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
 
 def principal():
-    st.title('Dashboard Blossom')
+    st.title('Dashboard Teste')
+    df = pd.DataFrame(np.random.randn(200, 3), columns=['a', 'b', 'c'])
+    c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c')
+    st.altair_chart(c)
 
 
 if __name__ == "__main__":
