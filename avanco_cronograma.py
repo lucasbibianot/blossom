1e114a4cb05131ecf7edec0d9a0d25cdb13e093c
Lucas Bibiano <lucas.bibianot@gmail.com>
2020-04-30 08:59:37 -0300

    Iniciando a exploração dos dados.

--- avanco_cronograma.py
+++ avanco_cronograma.py
@@ -2,6 +2,7 @@ import uuid
 import shutil
 import streamlit as st
 import pandas as pd
+import numpy as np
 import ler_pdf
 import os
 
@@ -15,22 +16,44 @@ def rodape():
 
 
 @st.cache
-def construir_dataframe(file_v1, file_v2):
+def carregar_cronograma(file_v1, file_v2):
     df_1 = ler_pdf.carregar_dataframe(file_v1)
     df_2 = ler_pdf.carregar_dataframe(file_v2)
     df_2.columns = ['cod_tarefa', 'descricao_atual',
                     'start_atual', 'end_atual']
-    df_join = df_1.set_index('cod_tarefa').join(df_2.set_index('cod_tarefa'))
+    return df_1.set_index('cod_tarefa').join(df_2.set_index('cod_tarefa'))
+
+
+def calcular_dias(row):
+    if pd.notna(row['end_atual']):
+        row['qtde_dias'] = np.timedelta64(row['end_atual'] - row['end'])
+        row['percentual_impacto'] = (row['qtde_dias'] / np.timedelta64(7, 'D')) 
+    else:
+        row['qtde_dias'] = 0
+        row['percentual_impacto'] = 0
+    return row
+
+
+def tratamento_dados(df_join):
     df_filtro = df_join[df_join['end_atual'] != df_join['end']]
-    st.dataframe(df_filtro)
+    df_filtro.drop(['descricao_atual'], axis=1, inplace=True)
+    df_filtro = df_filtro.apply(calcular_dias, axis=1)
+    filtro_concluidos = df_filtro['qtde_dias'] == 0
+    st.subheader('Tarefas concluídas no período')
+    st.dataframe(df_filtro[filtro_concluidos])
+    st.subheader('Tarefas em andamento')
+    st.dataframe(df_filtro[~filtro_concluidos])
+    media_reprogramacao = df_filtro[~filtro_concluidos].qtde_dias.mean()
+    st.write(f'A reprogramação acarretou em um avanço médio de {media_reprogramacao} dias')
 
 
 def sidebar_parametros():
     tmpdirname = f'/tmp/{uuid.uuid4()}'
+    df = None
     os.mkdir(tmpdirname)
     fp_v1 = f'{tmpdirname}/v1.pdf'
     fp_v2 = f'{tmpdirname}/v2.pdf'
-    data_avanco = st.sidebar.date_input('Selecione a data de avanço')
+    #data_avanco = st.sidebar.date_input('Selecione a data de avanço')
     uploaded_v1 = st.sidebar.file_uploader(
         "Selecione o PDF inicial", type="pdf")
     if uploaded_v1 is not None:
@@ -41,13 +64,17 @@ def sidebar_parametros():
         if uploaded_v2 is not None:
             with open(fp_v2, 'wb') as writer:
                 writer.write(uploaded_v2.read())
-                construir_dataframe(fp_v1, fp_v2)
+                if st.sidebar.button('Analisar'):
+                    df = carregar_cronograma(fp_v1, fp_v2)
     shutil.rmtree(tmpdirname)
+    return df
 
 
 def main():
     cabecalho()
-    sidebar_parametros()
+    df = sidebar_parametros()    
+    if df is not None:
+        tratamento_dados(df)
     rodape()
 
 
